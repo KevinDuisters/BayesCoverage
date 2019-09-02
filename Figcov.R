ea293446aed1299423f1af60e9c6b35888a240ea
Kevin Duisters <kevin.duisters@gmail.com>
2019-09-02 09:54:40 +0200

    update post summer

--- Figcov.R
+++ Figcov.R
@@ -16,8 +16,7 @@ source("functions/coverage.R")
 # Plot Coverage
 alpha <- 0.05
 w <- 1
-w=0.2
-#w=0.2 # Supplemental
+# w <- 0.2 # Supplemental
 
 if(w==1){pdf("Figures/figcov.pdf",width=9)}else{pdf("Figures/SFcov.pdf",width=9)}
 par(mfcol=c(2,2),xpd=F,mar=c(4,4,2,2))
@@ -39,8 +38,8 @@ par(mfrow=c(2,2),xpd=F,mar=c(4,4,2,2))
 lambdas <- c(0.75,7.5)
 for(lambda in lambdas){
   thetaseq <- seq(lambda+0.005,lambda+10,0.005)
-  coverage(thetaseq,alpha,lambda,w,dist="t5",plot.cov=T,cols=c("black","red","blue","orange","green")) 
-  #coverage(thetaseq,alpha,lambda,w,dist="t3",plot.cov=T,cols=c("black","red","blue","orange","green")) 
+  #coverage(thetaseq,alpha,lambda,w,dist="t5",plot.cov=T,cols=c("black","red","blue","orange","green")) 
+  coverage(thetaseq,alpha,lambda,w,dist="t3",plot.cov=T,cols=c("black","red","blue","orange","green")) 
   #coverage(thetaseq,alpha,lambda,w,dist="Cauchy",plot.cov=T,cols=c("black","red","blue","orange","green"))
 }
 
