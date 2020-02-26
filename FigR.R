74be60ddbe979d43ba5a9bf0199ee197e594c11e
Kevin Duisters <kevin.duisters@gmail.com>
2020-02-26 14:31:35 +0100

    trick to get figure dimensions in maniscript exactly right: plot single figure in panel of 3 as well, wit 2 blanks

--- FigR.R
+++ FigR.R
@@ -29,44 +29,46 @@ R2f <- function(x,alpha,w,lambda){Ginv(pmax(0,pmin(1,1 - alpha - (1-w)/w*alpha*g
 R3f <- function(x,alpha,w,lambda){Ginv(pmax(0,pmin(1,1 - alpha/2 - (1-w)/(2*w)*alpha*g(x)+(alpha/2)*(G(lambda-x)-G(-lambda-x)))))}
 
 #--------------------------------------------------------------------------------------------------------------------#
-# Settings for plot
-alpha <- 0.05
-lambda <- 5
-w <- 1
 
 
-pdf("Figures/figR.pdf",width=4,height=4.5)
-
-xgrid <- seq(0,15,0.0001)
-ranges <- c(0,15)
-
-par(mfrow=c(1,1))
-
-# get R
-R1 <- R1f(xgrid,alpha,w,lambda)
-R2 <- R2f(xgrid,alpha,w,lambda)
-R3 <- R3f(xgrid,alpha,w,lambda)
+#--------------------------------------------------------------------------------------------------------------------#
+# codechunk for plotting
+code.chunk <- function(lambda,alpha,w,distname,h=0.0001){
   
-plot(xgrid,xgrid,type="n",xlab="x",ylab="R",xlim=ranges,ylim=c(0,10)) 
-title(bquote(paste(.(distname),", ",lambda==.(lambda),", ",w==.(w))))
-
+  xgrid <- seq(0,15,h)
+  ranges <- c(0,10)
   
-X1 <- xgrid[xgrid > lambda + R1]
-X2 <- xgrid[(xgrid > 0) & (-lambda + R3 < xgrid) & (xgrid <= lambda + R1)  ]
-X3 <- xgrid[abs(xgrid) <= -lambda + R3]
-X4 <- -X2
-X5 <- -X1
-
+  # get R
+  R1 <- R1f(xgrid,alpha,w,lambda)
+  R2 <- R2f(xgrid,alpha,w,lambda)
+  R3 <- R3f(xgrid,alpha,w,lambda)
+  
+  plot(xgrid,xgrid,type="n",xlab="x",ylab="R",xlim=ranges,ylim=c(0,10)) 
+  title(bquote(paste(.(distname),", ",lambda==.(lambda),", ",w==.(w))))
+  
+  
+  X1 <- xgrid[xgrid > lambda + R1]
+  X2 <- xgrid[(xgrid > 0) & (-lambda + R3 < xgrid) & (xgrid <= lambda + R1)  ]
+  X3 <- xgrid[abs(xgrid) <= -lambda + R3]
+  X4 <- -X2
+  X5 <- -X1
+  
   # exclude |x| <= t_alpha
   ta <-max(0,xgrid[((w/(1-w))*(G(xgrid - lambda) + G(-xgrid - lambda))/g(xgrid)) <= (alpha/(1-alpha))])
   
   if(ta==0){ # this is a manual trick, not exact
-  abline(v=c(max(X5),max(X4),min(X2),min(X1)),lty=3,col="darkgrey")
-  mtext(c("I","IV","III","II","I"),side=1,adj=0.5,line=-1.5,at=c((min(ranges)+max(X5))/2,(max(X5)+max(X4))/2,(max(X4)+min(X2))/2,(min(X2)+min(X1))/2,(min(X1)+max(ranges))/2))
+    #abline(v=c(max(X5),max(X4),min(X2),min(X1)),lty=3,col="darkgrey")
+    #mtext(c("I","IV","III","II","I"),side=1,adj=0.5,line=-1.5,at=c((min(ranges)+max(X5))/2,(max(X5)+max(X4))/2,(max(X4)+min(X2))/2,(min(X2)+min(X1))/2,(min(X1)+max(ranges))/2))
+    abline(v=c(min(X2),min(X1)),lty=3,col="darkgrey")
+    mtext(c("III","II","I"),side=1,adj=0.5,line=-1.5,at=c((max(X4)+min(X2))/2,(min(X2)+min(X1))/2,(min(X1)+max(ranges))/2))
+    
   }else{
-    abline(v=c(max(X5),-ta,ta,min(X1)),lty=3,col="darkgrey")  
-    mtext(c("I","IV","-","II","I"),side=1,adj=0.5,line=-1.5,at=c((min(ranges)+max(X5))/2,(max(X5)-ta)/2,(-ta+ta)/2,(ta+min(X1))/2,(min(X1)+max(ranges))/2))
-    mtext(c(expression(-t[alpha]),expression(t[alpha])),side=1,at=c(-ta,ta),line=1)
+    # abline(v=c(max(X5),-ta,ta,min(X1)),lty=3,col="darkgrey")  
+    # mtext(c("I","IV","-","II","I"),side=1,adj=0.5,line=-1.5,at=c((min(ranges)+max(X5))/2,(max(X5)-ta)/2,(-ta+ta)/2,(ta+min(X1))/2,(min(X1)+max(ranges))/2))
+    # mtext(c(expression(-t[alpha]),expression(t[alpha])),side=1,at=c(-ta,ta),line=1)
+    abline(v=c(ta,min(X1)),lty=3,col="darkgrey")  
+    mtext(c("-","II","I"),side=1,adj=0.5,line=-1.5,at=c((-ta+ta)/2,(ta+min(X1))/2,(min(X1)+max(ranges))/2))
+    mtext(c(expression(t[alpha])),side=1,at=c(ta),line=1)
   }
   
   lines(xgrid,R1,col="green",lty=2)
@@ -78,7 +80,19 @@ X5 <- -X1
   lines(X2[abs(X2)>ta],R2f(X2[abs(X2)>ta],alpha,w,lambda),col="red",lty=1)
   lines(X3[abs(X3)>ta],R3f(X3[abs(X3)>ta],alpha,w,lambda),col="blue",lty=1)
   
+  
+}
+# note, could use this code to generate L,U figures as well (replace getU function). Gives the same results.
+
+#--------------------------------------------------------------------------------------------------------------------#
+# Visualize R function
+pdf("Figures/figR.pdf",width=12,height=4.5)
+
+par(mfrow=c(1,3)) # this is a trick to get the size exactly the same as figLU
+plot.new()
+code.chunk(lambda=5,alpha=0.05,w=1,distname) # the actual figure
+plot.new()
 
 dev.off()
 
-# note, could use this code to generate L,U figures as well (replace getU function). Gives the same results
+#--------------------------------------------------------------------------------------------------------------------#
